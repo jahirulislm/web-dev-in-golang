0e43b65dd48e34aecb8171b42fdeabce139afdfc
devjahirulislm <jhu@DESKTOP-03U6OT9.localdomain>
2025-12-14 23:52:39 +0600

    Initial commit

---
+++ helloWorld/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600) //The octal integer literal 0600, passed as the third parameter to WriteFile, indicates that the file should be created with read-write permissions for the current user only. (See the Unix man page open(2) for details.)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	page_1 := &Page{Title: "Golang", Body: []byte("Hello world with golang web dev!")}
+
+	page_1.save()
+
+	page_2, _ := loadPage("Golang")
+	fmt.Println(string(page_2.Body))
+
+	fmt.Println("successfully printed first golang programme.")
+}
